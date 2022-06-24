d630cc96b68a4fbd0d7f87b9decf03b39a540a37
Pongsathorn Keaonongyang <90130040+TOUCHNAJA@users.noreply.github.com>
2022-06-24 17:34:32 +0700

    Update Firestore.js

--- Firestore.js
+++ Firestore.js
@@ -4,13 +4,7 @@ import 'firebase/compat/auth';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXiWeJcehtpyWGb-0GuKhf11sZhPnWMW4",
-  authDomain: "todolist-3cc73.firebaseapp.com",
-  projectId: "todolist-3cc73",
-  storageBucket: "todolist-3cc73.appspot.com",
-  messagingSenderId: "138529154144",
-  appId: "1:138529154144:web:648b22c5e5ff5257dacf05",
-  measurementId: "G-37GMYFHZ1J"
+  
 };
 
 // Initialize Firebase
